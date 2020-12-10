f76a6c831d1e8794db1941027f87b50465a10379
Connor <c.mccurrie@gmail.com>
2020-12-10 12:09:30 -0600

    Contiuning UI changes

--- Program.cs
+++ Program.cs
@@ -30,6 +30,11 @@ namespace NorthwindConsole
                     Console.WriteLine("6) Edit Product");
                     Console.WriteLine("7) Display all Products");
                     Console.WriteLine("8) Display specific product");
+                    Console.WriteLine("9) Edit Category");
+                    Console.WriteLine("10) Display all Categories and their active products");
+                    Console.WriteLine("11) Display a specific Category and its active products");
+                    Console.WriteLine("12) Delete Category");
+                    Console.WriteLine("13) Delete Product");
                     Console.WriteLine("\"q\" to quit");
                     choice = Console.ReadLine();
                     Console.Clear();
@@ -111,19 +116,142 @@ namespace NorthwindConsole
                     //1.b Edit a specified record from the Products table
                     else if (choice == "6")
                     {
-                        
+                        Console.WriteLine("Choose the product to edit:");
+                        var db = new NorthwindConsole_32_CDMContext();
+                        var product = GetProducts(db);
+                        if (product != null)
+                        {
+                            Product EditProduct = InputProduct(db);
+                            if (EditProduct != null)
+                            {
+                                EditProduct.ProductId = product.ProductId;
+                                db.EditProduct(EditProduct);
+                                logger.Info($"Product (id: {product.ProductId})");
+                            }
+                        }
                     }
                     //1.c  Display all records in the Products table (ProductName only) - user decides if they want to see all products, discontinued products, or active (not discontinued) products. Discontinued products should be distinguished from active products.
 
                     else if (choice == "7")
                     {
+                        string pChoice;
+                        Console.WriteLine("Please choose which products you'd like to see:");
+                        Console.WriteLine("1) All Products");
+                        Console.WriteLine("2) Only Discontinued Products");
+                        Console.WriteLine("3) Only Active Products");
+                        pChoice = Console.ReadLine();
 
+
+                        if (pChoice == "1")
+                        {
+                            var db = new NorthwindConsole_32_CDMContext();
+                            var product = db.Products.OrderBy(p => p.ProductName);
+                            if (product != null)
+                            {
+                                Console.WriteLine($"There are {product.Count()} products in the database:");
+                                foreach (var allProds in product)
+                                {
+                                    Console.Write(allProds.ProductId+ " ");
+                                    Console.WriteLine(allProds.ProductName);
+                                }
+                            }
+                        } 
+                        else if (pChoice == "2")
+                        {
+                            var db = new NorthwindConsole_32_CDMContext();
+                            var products = db.Products.Where(p => p.Discontinued == true).OrderBy(p => p.ProductName);
+                            //var discontinued = db.Products.OrderBy(d => d.Discontinued);
+                            // if (discontinued != null)
+                            // {
+                                Console.WriteLine($"There are {products.Count()} discontinued products in the database:");
+                                foreach (var p in products)
+                                {
+
+                                    Console.WriteLine(p.ProductName);
+
+                                }
+                            // }
+                        }
+
+                        else if (pChoice == "3")
+                        {
+                            var db = new NorthwindConsole_32_CDMContext();
+                            var products = db.Products.Where(p => p.Discontinued == false).OrderBy(p => p.ProductName);
+                            Console.WriteLine($"There are {products.Count()} active products in the database:");
+                                foreach (var p in products)
+                                {
+                                    Console.WriteLine(p.ProductName);   
+                                }
+                        }
                     }
                     //1.d Display a specific Product (all product fields should be displayed)
                     else if (choice == "8")
                     {
-
+                        var db = new NorthwindConsole_32_CDMContext();
+                        Console.WriteLine("Type in the name of the product to display");
+                        string userInput = Console.ReadLine();
+                        var products = db.Products.Where(p => p.ProductName.Contains(userInput)).OrderBy(p => p.ProductName);
+                        Console.WriteLine($"There are {products.Count()} products that match your entry");
+                        foreach (var p in products)
+                        {
+                            Console.WriteLine($"{p.ProductId} - {p.ProductName} - {p.SupplierId} - {p.CategoryId} - {p.QuantityPerUnit} - {p.UnitPrice} - {p.UnitsInStock} - {p.UnitsOnOrder} - {p.ReorderLevel} - {p.Discontinued}");
+                        }
                     }
+                    //"9) Edit Category"
+                    else if (choice == "9")
+                    {
+                        Console.WriteLine("Choose the category to edit:");
+                        var db = new NorthwindConsole_32_CDMContext();
+                        var categories = GetCategories(db);
+                        if (categories != null)
+                        {
+                            // input blog
+                            Categories editCategory = InputCategories(db);
+                            if (editCategory != null)
+                            {
+                                editCategory.CategoryId = categories.CategoryId;
+                                db.EditCategory(editCategory);
+                                logger.Info($"Category (id: {categories.CategoryId}) updated");
+                            }
+                        }
+                    }
+                    
+                    // /Display all Categories and their active products
+                    else if (choice == "10")
+                    {
+                        
+                    }
+                    //Display a specific Category and its active products
+                    else if (choice == "11")
+                    {
+                        
+                    }
+                    //Delete Category
+                    else if (choice == "12")
+                    {
+                        Console.WriteLine("Choose Category to delete");
+                        var db = new NorthwindConsole_32_CDMContext();
+                        var catDelete = GetCategories(db);
+                        Categories categories = new Categories();
+                            if (catDelete != null)
+                            {
+                                db.DeleteCategory(catDelete);
+                                logger.Info($"Category (id: {catDelete.CategoryId}) deleted");
+                            }
+                    }
+                    //Delete Product
+                    else if (choice == "13")
+                    {
+                      Console.WriteLine("Choose Product to delete");
+                        var db = new NorthwindConsole_32_CDMContext();
+                        var prodDelete = GetProducts(db);
+                        if (prodDelete != null)
+                        {
+                            db.DeleteProduct(prodDelete);
+                            logger.Info($"Product (id: {prodDelete.ProductId}) deleted");
+                        }
+                    }
+
                     Console.WriteLine();
 
                 } while (choice.ToLower() != "q");
@@ -222,19 +350,9 @@ namespace NorthwindConsole
         {
 
             Product products = new Product();
-            Categories categories = new Categories();
             Console.WriteLine("Enter Product Name");
             products.ProductName = Console.ReadLine();
-            Console.WriteLine("Enter Related Category");
-            var query = db.Categories.OrderBy(p => p.CategoryName);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"{query.Count()} records returned");
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            foreach (var item in query)
-                {
-                    Console.WriteLine($"{item.CategoryName} - {item.Description}");
-                }
-            categories.CategoryName = Console.ReadLine();
+
             ValidationContext context = new ValidationContext(products, null, null);
             List<ValidationResult> results = new List<ValidationResult>();
 
